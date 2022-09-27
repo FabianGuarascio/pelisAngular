00dca8a7caa44a909e1a4d7f01dcaa62e751e3ca
FabianGuarascio <guarascio.fabiand@gmail.com>
2022-09-26 23:08:35 -0300

    Creado el homeComponent e importado el shared module en home module al mismo tiempo que agrego la ruta de homeModule

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
 
-
+const routes=[
+  {
+  path:"",
+  component:HomeComponent
+  }
+]
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class HomeModule { }
