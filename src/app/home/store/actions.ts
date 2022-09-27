4d912ca14d975bc4cf856e1651978902ad3a2c1b
FabianGuarascio <guarascio.fabiand@gmail.com>
2022-09-27 14:12:27 -0300

    modificado el nombre final de cada accion

--- src/app/home/store/actions.ts
+++ src/app/home/store/actions.ts
@@ -1,13 +1,13 @@
 import { createAction, props } from "@ngrx/store";
 import { IResult } from "../types/movieResponse.interface";
 
-export const getBooks=createAction(
+export const getBooksAction=createAction(
     '[Home Page] Get Books'
 )
-export const getBookSuccess=createAction(
+export const getBookSuccessAction=createAction(
     '[Home Page] Get Books success',
     props<{results:IResult[]}>()
 )
-export const getBookFailure=createAction(
+export const getBookFailureAction=createAction(
     '[Home Page] Get Books failure'
 )
