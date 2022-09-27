5267036872382becdd406bb559da977a38aff6df
FabianGuarascio <guarascio.fabiand@gmail.com>
2022-09-26 23:19:26 -0300

    generado un caso de prueba en home

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+public testArr=[1,2,3]
   constructor() { }
 
   ngOnInit(): void {
