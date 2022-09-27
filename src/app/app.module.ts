bb0fcd239952b8a34a8c49a657b8ad478e7bcf69
FabianGuarascio <guarascio.fabiand@gmail.com>
2022-09-26 22:53:14 -0300

    creado los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
+import { SharedModule } from './shared/shared.module';
+import { LoginModule } from './login/login.module';
+import { MovieDetailsModule } from './movie-details/movie-details.module';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
@@ -15,6 +19,10 @@ import { environment } from '../environments/environment';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    SharedModule,
+    LoginModule,
+    MovieDetailsModule,
+    HomeModule,
     StoreModule.forRoot({}, {}),
     EffectsModule.forRoot([]),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
