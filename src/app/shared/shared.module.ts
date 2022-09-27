860c8e9c48fc89c1456bb4f6560bf9494e7ccf61
FabianGuarascio <guarascio.fabiand@gmail.com>
2022-09-26 23:07:14 -0300

    Creado componentes del sharedModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MoviePosterComponent } from './movie-poster/movie-poster.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MoviePosterComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    MoviePosterComponent,
+    HeaderComponent,
+    FooterComponent
   ]
 })
 export class SharedModule { }
