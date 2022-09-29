a79a88b1c3d546cb134d81802ed86a91a1436bbc
FabianGuarascio <guarascio.fabiand@gmail.com>
2022-09-29 20:45:01 -0300

    se implemento ahora el store

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { IAppState } from 'src/app/types/app-state';
+import { MovieService } from '../services/movie.service';
+import { getMoviesAction } from '../store/actions';
+import { getMoviesLoadingSelector, getMoviesSuccessSelector } from '../store/selectors';
+import { IResult } from '../types/movieResponse.interface';
+import {Observable} from 'rxjs'
 
 @Component({
   selector: 'app-home',
@@ -6,10 +13,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-public testArr=[1,2,3]
-  constructor() { }
+public isLoading$= this.store.pipe(select(getMoviesLoadingSelector))
+public movies$:Observable<IResult[]>= this.store.pipe(select(getMoviesSuccessSelector))
+  
+  constructor(private store:Store<IAppState>) {
+    this.store.dispatch(getMoviesAction())
+    }
 
   ngOnInit(): void {
+
   }
 
 }
--- src/app/shared/movie-poster/movie-poster.component.ts
+++ src/app/shared/movie-poster/movie-poster.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { IResult } from 'src/app/home/types/movieResponse.interface';
 
 @Component({
   selector: 'app-movie-poster',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-poster.component.scss']
 })
 export class MoviePosterComponent implements OnInit {
+  @Input() movie?:IResult | null
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.movie)
   }
 
 }
