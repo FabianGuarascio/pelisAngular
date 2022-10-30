33009e44cb24fdb8d4ae11c024c4971193754fa9
FabianGuarascio <guarascio.fabiand@gmail.com>
2022-10-30 15:58:10 -0300

    Added the favMovies Effects

--- src/app/home/store/effects.ts
+++ src/app/home/store/effects.ts
@@ -1,8 +1,8 @@
 import {Injectable} from '@angular/core'
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { MovieService } from '../services/movie.service';
-import { getMoviesAction, getMoviesSuccessAction } from './actions';
-import{mergeMap, map}from 'rxjs'
+import { addFavMovieAction, addFavMovieFailureAction, addFavMovieSuccessAction, deleteFavMovieAction, deleteFavMovieFailureAction, deleteFavMovieSuccessAction, getFavMoviesAction, getFavMoviesFailureAction, getFavMoviesSuccessAction, getMoviesAction, getMoviesFailureAction, getMoviesSuccessAction } from './actions';
+import{mergeMap, map, catchError, of, tap}from 'rxjs'
 @Injectable()
 export class MovieEffects{
 
@@ -14,9 +14,48 @@ getMovies$=createEffect(()=>
             .pipe(
                 map(movies=>(getMoviesSuccessAction({results:movies})))
             )
-        )
+        ),
+        catchError((error)=>of(getMoviesFailureAction({error:error})))
     )
 )
+getFavMovies$=createEffect(()=>
+    this.actions$.pipe(
+        ofType(getFavMoviesAction),
+        mergeMap(
+            ()=> this._movieService.getAllFavMovies()
+            .pipe(
+                map(favMovies=>(getFavMoviesSuccessAction({favMovies:favMovies})))
+            )
+        ),
+        catchError((error)=>of(getFavMoviesFailureAction({error:error})))
+    )
+)
+addFavMovie$ = createEffect(()=>
+    this.actions$.pipe(
+        ofType(addFavMovieAction),
+        mergeMap(
+            (action)=> this._movieService.addFavMovie(action.favMovie)
+            .pipe(
+                map(()=>addFavMovieSuccessAction())
+            )
+        ),
+        catchError((error)=>of(addFavMovieFailureAction({error:error})))
+    )
+)
+
+deleteFavMovie$ = createEffect(()=>
+this.actions$.pipe(
+    ofType(deleteFavMovieAction),
+    mergeMap(
+        (action)=> this._movieService.deleteFavMovie(action.id).pipe(
+            map(()=>deleteFavMovieSuccessAction())
+        )
+    ),
+    catchError((error)=>of(deleteFavMovieFailureAction({error:error})))
+))
+
+
+
 
     constructor(
         private _movieService:MovieService,
