205696723c45be578c912d542f6fecc6a740294b
FabianGuarascio <guarascio.fabiand@gmail.com>
2022-10-30 15:57:39 -0300

    Added the fav  Movies actions

--- src/app/home/store/actions.ts
+++ src/app/home/store/actions.ts
@@ -1,4 +1,5 @@
 import { createAction, props } from "@ngrx/store";
+import { IFavMovie } from "../types/favMovies.interface";
 import { IResult } from "../types/movieResponse.interface";
 
 export const getMoviesAction=createAction(
@@ -15,5 +16,48 @@ export const addDummyData=createAction(
     '[Home Page] add dummy movie success'
 )
 export const getMoviesFailureAction=createAction(
-    '[Home Page] Get Movies failure'
+    '[Home Page] Get Movies failure',
+    props<{error:string}>()
 )
+
+export const getFavMoviesAction=createAction(
+    '[Fav Page] Get Fav Movies'
+)
+export const getFavMoviesSuccessAction=createAction(
+    '[Fav Api] Get Fav Movies Success',
+    props<{favMovies:IFavMovie[]}>()
+)
+export const getFavMoviesFailureAction=createAction(
+    '[Fav Api] Get Fav Movies failure',
+    props<{error:string}>()
+)
+
+export const addFavMovieAction=createAction(
+    '[Fav Page] Add Fav Movie',
+    props<{favMovie:IFavMovie}>()
+)
+
+export const addFavMovieSuccessAction=createAction(
+    '[Fav Api] Add Fav Movie Success',
+)
+
+export const addFavMovieFailureAction=createAction(
+    '[Fav Page] Add Fav Movie Failure',
+    props<{error:string}>()
+)
+
+export const deleteFavMovieAction=createAction(
+    '[Fav Page] Remove Fav Movie',
+    props<{id:string}>()
+)
+export const deleteFavMovieSuccessAction=createAction(
+    '[Fav Page] Remove Fav Movie Success'
+)
+
+export const deleteFavMovieFailureAction=createAction(
+    '[Fav Page] Remove Fav Movie Failure',
+    props<{error:string}>()
+)
+
+
+
