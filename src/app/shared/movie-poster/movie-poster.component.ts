c5a66a89964ef85244538d5dc09d84b8c605722a
FabianGuarascio <guarascio.fabiand@gmail.com>
2022-10-30 16:06:17 -0300

    Created the movie poster component and added the ngrx fucntionalities

--- src/app/shared/movie-poster/movie-poster.component.ts
+++ src/app/shared/movie-poster/movie-poster.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { addFavMovieAction } from 'src/app/home/store/actions';
+import { IFavMovie } from 'src/app/home/types/favMovies.interface';
 import { IResult } from 'src/app/home/types/movieResponse.interface';
+import { IAppState } from 'src/app/types/app-state';
 
 @Component({
   selector: 'app-movie-poster',
@@ -8,10 +12,19 @@ import { IResult } from 'src/app/home/types/movieResponse.interface';
 })
 export class MoviePosterComponent implements OnInit {
   @Input() movie?:IResult | null
-
-  constructor() { }
+  public posterPath:string=""
+  constructor(private store:Store<IAppState>) { }
 
   ngOnInit(): void {
+    this.posterPath = 'https://image.tmdb.org/t/p/w200'+this.movie?.poster_path
+  }
+
+  addToFav(title:any,img:any){
+    let body:IFavMovie = {
+      title:title,
+      img:img
+    }
+    this.store.dispatch(addFavMovieAction({favMovie:body}))
   }
 
 }
