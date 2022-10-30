935c1032f410edac275fe0455272228da89b9d2d
FabianGuarascio <guarascio.fabiand@gmail.com>
2022-10-30 15:57:22 -0300

    Added som http methos for the favMovies crud

--- src/app/home/services/movie.service.ts
+++ src/app/home/services/movie.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import {Observable, map} from 'rxjs'
 import { environment } from 'src/environments/environment';
+import { IFavMovie } from '../types/favMovies.interface';
 import { IMovieServiceResponse, IResult } from '../types/movieResponse.interface';
 
 @Injectable({
@@ -12,9 +13,22 @@ export class MovieService {
   constructor(private http:HttpClient) { }
 
   getMovies():Observable<IResult[]>{
-   return this.http.get<IMovieServiceResponse>(environment.urlApi)
-   .pipe(
-    map((movieResponse:IMovieServiceResponse)=>movieResponse.results)
-   )
+    return this.http.get<IMovieServiceResponse>(environment.urlApi)
+    .pipe(
+      map((movieResponse:IMovieServiceResponse)=>movieResponse.results)
+    )
+  }
+  
+  getAllFavMovies():Observable<any>{
+    return this.http.get(`${environment.nodeUrl}`)
+  }
+
+  addFavMovie(body:IFavMovie):Observable<any>{
+    return this.http.post(`${environment.nodeUrl}`,body)
+  }
+
+
+  deleteFavMovie(id:string){
+    return this.http.delete(`${environment.nodeUrl}/${id}`)
   }
 }
