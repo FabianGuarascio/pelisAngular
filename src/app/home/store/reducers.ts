386a8ebf8e6681133385cce2581eb2ce96c09090
FabianGuarascio <guarascio.fabiand@gmail.com>
2022-10-30 15:57:55 -0300

    Added the favmovies reducers

--- src/app/home/store/reducers.ts
+++ src/app/home/store/reducers.ts
@@ -1,7 +1,22 @@
 import { createReducer, on } from "@ngrx/store";
+import { IFavMovieState } from "src/app/home/types/favMovieState.interface";
 import { IResult } from "../types/movieResponse.interface";
 import { IMovieState } from "../types/movieState.interface";
-import { addDummyData, getMoviesAction, getMoviesSuccessAction, removeMoviesSuccessAction } from "./actions";
+import { 
+    addDummyData,
+    addFavMovieAction,
+    addFavMovieFailureAction,
+    addFavMovieSuccessAction,
+    deleteFavMovieAction,
+    deleteFavMovieFailureAction,
+    deleteFavMovieSuccessAction,
+    getFavMoviesAction,
+    getFavMoviesFailureAction,
+    getFavMoviesSuccessAction,
+    getMoviesAction,
+    getMoviesSuccessAction,
+    removeMoviesSuccessAction
+    } from "./actions";
 
 export const initialState:IMovieState={
     isLoading:false,
@@ -43,5 +58,66 @@ export const movieReducer=createReducer(
     on(addDummyData,(state)=>({
         ...state,
         results: state.results.concat(dummyData)
+    }))
+)
+
+const favInitialState:IFavMovieState={
+    isLoading:false,
+    hasLoaded:false,
+    favMovies:[],
+    deleteId:"",
+    error:null
+}
+export const favMovieReducer=createReducer(
+    favInitialState,
+    on(getFavMoviesAction,(state)=>({
+        ...state,
+        isLoading:true
+    })),
+    on(getFavMoviesSuccessAction,(state,{favMovies})=>({
+        ...state,
+        isLoading:false,
+        hasLoaded:true,
+        favMovies:favMovies
+    })),
+    on(getFavMoviesFailureAction,(state,{error})=>({
+        ...state,
+        isLoading:false,
+        error:error
+    })),
+    on(addFavMovieAction,(state,{favMovie})=>({
+        ...state,
+        isLoading:true,
+        currentFavMovie:favMovie
+    })),
+    on(addFavMovieSuccessAction,(state,result)=>({
+        ...state,
+        favMovies:[...state.favMovies,state.currentFavMovie!],
+        isLoading:false,
+        hasLoaded:true,
+        currentFavMovie:undefined
+    })),
+    on(addFavMovieFailureAction,(state,{error})=>({
+        ...state,
+        isLoading:false,
+        currentFavMovie:undefined,
+        error:error
+    })),
+    on(deleteFavMovieAction,(state,{id})=>({
+        ...state,
+        isLoading:true,
+        deleteId:id
+    })),
+    on(deleteFavMovieSuccessAction,(state)=>({
+        ...state,
+        isLoading:false,
+        favMovies: state.favMovies.filter(favMovie=> favMovie._id!==state.deleteId),
+        deleteId:""
+    })),
+    on(deleteFavMovieFailureAction,(state,{error})=>({
+        ...state,
+        isLoading:false,
+        deleteId:"",
+        error:error
     }))
 )
